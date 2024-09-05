90b8a38357861e6a3939714c7edc123dd1f6fc32
ellis0808 <ellis.jaiye@gmail.com>
2024-09-05 22:16:00 +0900

    server test

--- api/login-check.php
+++ api/login-check.php
@@ -4,7 +4,7 @@ session_start();
 
 if (!isset($_SESSION['user_id'])) {
     // Redirect to login page
-    header('Location: /resources/login/login.html');
+    header('Location: GPSEnglishPractice-test/resources/login/login.html');
     exit();
     echo json_encode(['message' => 'not logged in']);
 }
