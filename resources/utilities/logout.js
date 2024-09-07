27a6bbcf424aae41cd256343c7bf61b35f70db20
ellis0808 <ellis.jaiye@gmail.com>
2024-09-07 19:16:21 +0900

    server test

--- resources/utilities/logout.js
+++ resources/utilities/logout.js
@@ -7,7 +7,8 @@ async function logout() {
     if (!response.ok) {
       throw new Error("Error loggin out", data.error);
     }
-    window.location.href = "/resources/login/login.html";
+    window.location.href =
+      "/KGPSEnglishPractice-test/resources/login/login.html";
   } catch (error) {
     console.error("Error loging out: ", error);
   }
