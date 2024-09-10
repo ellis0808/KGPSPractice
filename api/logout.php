34e35b614b4c05100b523e779b90ea261a888a2b
ellis0808 <ellis.jaiye@gmail.com>
2024-09-10 23:42:18 +0900

    testing using user data in frontend

--- api/logout.php
+++ api/logout.php
@@ -13,4 +13,4 @@ session_start();
 session_unset();
 session_destroy();
 
-echo json_encode(['message' => 'Logged out successfully']);
+echo json_encode(['loggedIn' => false]);
--- resources/utilities/logout.js
+++ resources/utilities/logout.js
@@ -2,13 +2,13 @@ async function logout() {
   try {
     const response = await fetch("/KGPSEnglishPractice-test/api/logout.php");
     const data = await response.json();
-    console.log(data);
 
     if (!response.ok) {
       throw new Error("Error loggin out", data.error);
+    } else {
+      window.location.href =
+        "/KGPSEnglishPractice-test/resources/login/login.html";
     }
-    window.location.href =
-      "/KGPSEnglishPractice-test/resources/login/login.html";
   } catch (error) {
     console.error("Error loging out: ", error);
   }
