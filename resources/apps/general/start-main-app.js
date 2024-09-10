f2a2d7f47fcfeb90e002c6102b2add1b4894534f
ellis0808 <ellis.jaiye@gmail.com>
2024-09-10 21:56:27 +0900

    testing using user data in frontend

--- resources/apps/general/start-main-app.js
+++ resources/apps/general/start-main-app.js
@@ -17,7 +17,6 @@ import {
 } from "./app-launcher.js";
 import { logout } from "/KGPSEnglishPractice-test/resources/utilities/logout.js";
 import { sessionCheck } from "/KGPSEnglishPractice-test/resources/login/session-check.js";
-import { checkSession } from "../../utilities/check-session.js";
 
 /*
 **********
@@ -100,7 +99,6 @@ const div4 = document.createElement("div");
 let isMainMenu = true;
 function startMainApp() {
   sessionCheck();
-  checkSession();
 
   stylesheet.setAttribute(
     "href",
--- resources/login/session-check.js
+++ resources/login/session-check.js
@@ -5,18 +5,17 @@ async function sessionCheck() {
       {
         method: "GET",
         headers: { "Content-Type": "application/json" },
-        credentials: "include",
+        credentials: "include", // sends cookies!
       }
     );
-    console.log("Session Check Response:", response);
 
-    const data = await response.json();
-    console.log(data);
+    const sessionData = await response.json();
+    console.log(sessionData);
 
-    if (data.loggedIn) {
-      console.log(`user id: ${data.user_id}`);
-      console.log(`first name: ${data.firstname}`);
-      console.log(`last name: ${data.lastname}`);
+    if (sessionData.loggedIn) {
+      console.log(`user id: ${sessionData.user_id}`);
+      console.log(`first name: ${sessionData.firstname}`);
+      console.log(`last name: ${sessionData.lastname}`);
     } else {
       // Redirect to login page
       if (
@@ -28,9 +27,13 @@ async function sessionCheck() {
       }
     }
   } catch (error) {
-    console.error("Error checking session:", error);
-    window.location.href =
-      "/KGPSEnglishPractice-test/resources/login/error.html";
+    if (
+      window.location.pathname !==
+      "/KGPSEnglishPractice-test/resources/login/login.html"
+    ) {
+      window.location.href =
+        "/KGPSEnglishPractice-test/resources/login/login.html";
+    }
   }
 }
 
