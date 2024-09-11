8d25c9d53589d7eb728d4cd0b24acac347d7cb82
ellis0808 <ellis.jaiye@gmail.com>
2024-09-11 23:11:24 +0900

    testing new database table data

--- api/logout.php
+++ api/logout.php
@@ -1,16 +1,16 @@
 <?php
 session_start();
 
-require './db_connect.php';
+$_SESSION = array();
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header("HTTP/1.1 204 No Content");
-    exit;
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
 }
 
 
 
-session_unset();
+
 session_destroy();
 
 echo json_encode(['loggedIn' => false]);
