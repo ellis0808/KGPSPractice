a17220212d9b7d1a9af5d6d220f1194ccf89ada5
ellis0808 <ellis.jaiye@gmail.com>
2024-09-14 22:03:11 +0900

    database test

--- api/update_user.php
+++ api/update_user.php
@@ -13,7 +13,7 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 error_log(print_r($data, true));
 
-$id = $data['student_id'] ?? null;
+$id = $data['id'] ?? null;
 
 if (!$id) {
     echo json_encode(['error' => 'User ID is required']);
@@ -66,7 +66,7 @@ try {
         if ($stmt->rowCount()) {
             echo json_encode(['message' => 'User updated successfully']);
         } else {
-            echo json_encode(['message' => 'No changes made or user not found1']);
+            echo json_encode(['message' => 'No changes made or user not found']);
         }
     } else {
         echo json_encode(['message' => 'No changes made or user not found2']);
