a93e0358a97d3ec135afdc8f89ace5fffc160576
ellis0808 <ellis.jaiye@gmail.com>
2024-09-14 13:32:50 +0900

    database test

--- resources/login/login.js
+++ resources/login/login.js
@@ -244,7 +244,7 @@ document
   .getElementById("studentPasswordEntryForm")
   .addEventListener("submit", (event) => {
     loginUser(
-      selectedUser.id,
+      selectedUser.student_id,
       selectedUser.firstname,
       selectedUser.lastname,
       selectedUser.access
