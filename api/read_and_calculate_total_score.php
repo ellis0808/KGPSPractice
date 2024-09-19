adfbd971df966d568a2d29a9fc7aa5aa8d56cd7d
ellis0808 <ellis.jaiye@gmail.com>
2024-09-19 23:41:05 +0900

    database test

--- api/read_and_calculate_total_score.php
+++ api/read_and_calculate_total_score.php
@@ -15,8 +15,8 @@ try {
         $stmt = $pdo->prepare('SELECT SUM(activity_score) AS total_score FROM activity_stats WHERE user_id = :user_id');
         $stmt->execute(['user_id' => $_GET['id']]);
         $userTotalScore = $stmt->fetch(PDO::FETCH_ASSOC)['total_score'];
-        if ($userTotalScore) {
-            echo json_encode($userTotalScore);
+        if ($userTotalScore !== null) {
+            echo json_encode(['total_score' => $userTotalScore]);
         } else {
             echo json_encode(['message' => 'User not found']);
         }
