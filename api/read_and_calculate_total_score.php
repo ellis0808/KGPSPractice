b5a64cc5fde050e1e15a35049f88d4f71c6f4d6e
ellis0808 <ellis.jaiye@gmail.com>
2024-09-19 23:35:14 +0900

    database test

--- api/read_and_calculate_total_score.php
+++ api/read_and_calculate_total_score.php
@@ -12,14 +12,17 @@ try {
     $pdo = getDBConnection();
     if (isset($_GET['id'])) {
 
-        $stmt = $pdo->prepare('SELECT SUM(activity_score) FROM activity_stats WHERE user_id = :user_id');
-        $stmt->execute(['id' => $_GET['id']]);
-        $userTotalScore = $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt = $pdo->prepare('SELECT SUM(activity_score) AS total_score FROM activity_stats WHERE user_id = :user_id');
+        $stmt->execute(['user_id' => $_GET['id']]);
+        $userTotalScore = $stmt->fetch(PDO::FETCH_ASSOC)['total_score'];
         if ($userTotalScore) {
             echo json_encode($userTotalScore);
         } else {
             echo json_encode(['message' => 'User not found']);
         }
+    } else {
+        echo json_encode(['error' => 'User ID not provided']);
+        exit;
     }
 } catch (PDOException $e) {
     echo json_encode(['error' => $e->getMessage()]);
