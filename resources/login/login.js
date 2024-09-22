dbbdb1d604c68f49b704de7a5303404e71a66e36
ellis0808 <ellis.jaiye@gmail.com>
2024-09-22 23:49:30 +0900

    adding images to student login grid

--- resources/login/login.js
+++ resources/login/login.js
@@ -171,7 +171,7 @@ passwordImageArray.forEach((image) => {
   gridImage.classList.add("grid-image");
   gridImage.setAttribute("id", passwordImageObject[image].id);
   gridImage.setAttribute("content", passwordImageObject[image].content);
-  gridImage.style.backgroundImage = `url(${[image].image})`;
+  gridImage.style.backgroundImage = `url(${image.image})`;
   // gridImage.innerText = `${passwordImageObject[image].content}`;
   gridImage.addEventListener("click", (event) => {
     // deselects previously selected grid image
