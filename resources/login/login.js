711020dd88e42566fdca8a2287c25c368bd51713
ellis0808 <ellis.jaiye@gmail.com>
2024-09-23 13:43:32 +0900

    adding images to student login grid

--- resources/login/login.js
+++ resources/login/login.js
@@ -200,24 +200,19 @@ studentPasswordGridContainer.appendChild(studentPasswordGrid);
 
 function displaySelectedPasswordImages() {
   if (studentPasswordEntryArray[0] !== null) {
-    // if (studentPasswordEntryArray[1] === null) {
-    // studentSelectedPasswordImage1.classList.add("selected");
     studentSelectedPasswordImage1.style.backgroundImage = `url(${
       passwordImageObject[studentPasswordEntryArray[0]].image
     })`;
     // }
   } else {
-    // studentSelectedPasswordImage1.classList.remove("selected");
-    studentSelectedPasswordImage1.innerText = ``;
+    studentSelectedPasswordImage1.style.backgroundImage = ``;
   }
   if (studentPasswordEntryArray[1] !== null) {
-    // studentSelectedPasswordImage2.classList.add("selected");
     studentSelectedPasswordImage2.style.backgroundImage = `url(${
       passwordImageObject[studentPasswordEntryArray[1]].image
     })`;
   } else {
-    // studentSelectedPasswordImage1.classList.remove("selected");
-    studentSelectedPasswordImage2.innerText = ``;
+    studentSelectedPasswordImage1.style.backgroundImage = ``;
   }
 }
 
