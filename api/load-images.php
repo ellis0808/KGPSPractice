bcbc4ef787c14976ba6f6930b0adadd0edb3732c
ellis0808 <ellis.jaiye@gmail.com>
2024-09-27 21:33:16 +0900

    image tagging page setup

--- api/load-images.php
+++ api/load-images.php
@@ -12,7 +12,7 @@ try {
     // Get all images with the indicated filename
     if (isset($_GET['id'])) {
         $stmt = $pdo->prepare('SELECT image_id, type, category, filename, filetype, link FROM images WHERE filename LIKE :item');
-        $stmt->execute(['item' => '%' . $_GET['id'] . '%']);
+        $stmt->execute(['item' => '%' . $_GET['id']]);
         $image = $stmt->fetchAll(PDO::FETCH_ASSOC);
         if ($image) {
             echo json_encode($image);
