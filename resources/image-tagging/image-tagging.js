1077920a48e29d5e94a67fbba17d518cc5efea25
ellis0808 <ellis.jaiye@gmail.com>
2024-09-27 20:57:44 +0900

    image tagging page setup

--- resources/image-tagging/image-tagging.js
+++ resources/image-tagging/image-tagging.js
@@ -11,39 +11,7 @@ async function loadImages() {
       "/KGPSEnglishPractice-test/api/load-images.php"
     );
     const imageData = await response.json();
-    console.log(imageData.images);
-
-    let i = 0;
-    imageData.images.forEach((item) => {
-      ++i;
-      const imageRow = document.createElement("div");
-      imageRow.classList.add("image-row");
-      const number = document.createElement("div");
-      const imageTypeContainer = document.createElement("div");
-      const imageCategoryContainer = document.createElement("div");
-      const imageFileNameContainer = document.createElement("div");
-      const imageFileTypeContainer = document.createElement("div");
-      const imageLinkContainer = document.createElement("div");
-      const imageThumbnailContainer = document.createElement("div");
-      number.classList.add("number");
-      imageThumbnailContainer.classList.add("thumbnail");
-      number.innerText = `${i}`;
-      imageTypeContainer.innerText = item.type;
-      imageCategoryContainer.innerText = item.category;
-      imageFileNameContainer.innerText = item.filename;
-      imageFileTypeContainer.innerText = item.filetype;
-      imageLinkContainer.innerText = item.link;
-      imageThumbnailContainer.style.backgroundImage = `url(${item.link})`;
-      imageRow.appendChild(number);
-      imageRow.appendChild(imageTypeContainer);
-      imageRow.appendChild(imageCategoryContainer);
-      imageRow.appendChild(imageFileNameContainer);
-      imageRow.appendChild(imageFileTypeContainer);
-      imageRow.appendChild(imageLinkContainer);
-      imageRow.appendChild(imageThumbnailContainer);
-      imageDataContainer.appendChild(imageRow);
-    });
-
+    populateImageTable(imageData);
     if (!response.ok) {
       throw new Error("Network response was not okay");
     }
@@ -75,8 +43,44 @@ async function imageSearch(searchItem) {
     }
 
     const data = await response.json();
-    console.log(data);
+    populateImageTable(data);
   } catch (error) {
     console.log("There was an error loading the images", error.message);
   }
+}
+
+function populateImageTable(data) {
+  let i = 0;
+  if (data.images) {
+    data = data.images;
+  }
+  data.forEach((item) => {
+    ++i;
+    const imageRow = document.createElement("div");
+    imageRow.classList.add("image-row");
+    const number = document.createElement("div");
+    const imageTypeContainer = document.createElement("div");
+    const imageCategoryContainer = document.createElement("div");
+    const imageFileNameContainer = document.createElement("div");
+    const imageFileTypeContainer = document.createElement("div");
+    const imageLinkContainer = document.createElement("div");
+    const imageThumbnailContainer = document.createElement("div");
+    number.classList.add("number");
+    imageThumbnailContainer.classList.add("thumbnail");
+    number.innerText = `${i}`;
+    imageTypeContainer.innerText = item.type;
+    imageCategoryContainer.innerText = item.category;
+    imageFileNameContainer.innerText = item.filename;
+    imageFileTypeContainer.innerText = item.filetype;
+    imageLinkContainer.innerText = item.link;
+    imageThumbnailContainer.style.backgroundImage = `url(${item.link})`;
+    imageRow.appendChild(number);
+    imageRow.appendChild(imageTypeContainer);
+    imageRow.appendChild(imageCategoryContainer);
+    imageRow.appendChild(imageFileNameContainer);
+    imageRow.appendChild(imageFileTypeContainer);
+    imageRow.appendChild(imageLinkContainer);
+    imageRow.appendChild(imageThumbnailContainer);
+    imageDataContainer.appendChild(imageRow);
+  });
 }
