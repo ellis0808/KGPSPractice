256159312248e233b5be1b007db43e1e62b6e1aa
ellis0808 <ellis.jaiye@gmail.com>
2024-09-30 20:32:10 +0900

    testing audio directory setup

--- resources/apps/card-touch/card-touch-index.js
+++ resources/apps/card-touch/card-touch-index.js
@@ -80,18 +80,20 @@ async function loadAudio(style) {
     const audioLinks = data.map((item) => {
       return item.link;
     });
-    try {
-      const response = await fetch(audioLinks);
-      if (!response.ok) {
-        throw new Error("Network response was not okay");
-      }
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.log("There was an error loading the audio", error);
-    }
+    // return audioLinks;
+    console.log(audioLinks);
   } catch (error) {
     console.log("There was an error ", error);
+  }
+  try {
+    const response = await fetch(audioLinks);
+    if (!response.ok) {
+      throw new Error("Network response was not okay");
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("There was an error loading the audio", error);
   }
 }
 
