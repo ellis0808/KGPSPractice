1594c307779d67b3862bb1577c455cf7c953c2e3
ellis0808 <ellis.jaiye@gmail.com>
2024-10-07 00:13:55 +0900

    load audio script improvements

--- resources/apps/card-touch/card-touch-audio.js
+++ resources/apps/card-touch/card-touch-audio.js
@@ -68,11 +68,11 @@ async function getAudio(style) {
   try {
     let response;
     if (grouping2 != null) {
-      const response = await fetch(
+      response = await fetch(
         `/KGPSEnglishPractice-test/api/load_audio.php?id1=${category}&id2=${grouping}&id3=${grouping2}`
       );
     } else if (grouping2 === null) {
-      const response = await fetch(
+      response = await fetch(
         `/KGPSEnglishPractice-test/api/load_audio.php?id1=${category}&id2=${grouping}`
       );
     }
