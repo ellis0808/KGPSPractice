87a0a188f64eeef803b640ba36a463f1f71a0e46
ellis0808 <ellis.jaiye@gmail.com>
2024-10-07 22:24:08 +0900

    load audio script improvements; abstracted out load audio to separate utilities file

--- resources/utilities/audio.js
+++ resources/utilities/audio.js
@@ -4,18 +4,26 @@ async function getAudio(category, grouping, grouping2) {
   try {
     let response;
     if (grouping2 !== null) {
+      console.log("test 1");
+
       response = await fetch(
         `/KGPSEnglishPractice-test/api/load_audio.php?id1=${category}&id2=${grouping}&id3=${grouping2}`
       );
+      console.log("test 2");
     } else if (grouping2 === null) {
+      console.log("test 3");
+
       response = await fetch(
         `/KGPSEnglishPractice-test/api/load_audio.php?id1=${category}&id2=${grouping}`
       );
+      console.log("test 5");
     }
     if (!response.ok) {
       throw new Error("Network response was not okay");
     }
     const audioData = await response.json();
+    console.log("test 6");
+
     console.log(audioData);
 
     loadAudio(audioData);
