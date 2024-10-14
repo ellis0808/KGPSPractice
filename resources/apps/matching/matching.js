cb75432ba27cf94562a54dc28bbd61e3b58ee361
Logan Jeffrey <ellis.jaiye@gmail.com>
2024-10-14 22:37:18 +0900

    matching app fix (lines persisting on exit)

--- resources/apps/matching/matching.js
+++ resources/apps/matching/matching.js
@@ -387,6 +387,7 @@ function endSession() {
   document.querySelectorAll(".letter-matching-app, .line").forEach((item) => {
     item.remove();
   });
+  console.log(document.querySelectorAll(".line"));
   if (document.querySelector(".end-messages-container")) {
     document.querySelector(".end-messages-container").remove();
   }
