ceb610eee597bb0c582c095fb0e69485d83f4843
Logan Jeffrey <ellis.jaiye@gmail.com>
2024-10-19 21:38:17 +0900

    Updating Main Menu layouts

--- resources/apps/general/start-main-app.js
+++ resources/apps/general/start-main-app.js
@@ -424,24 +424,9 @@ class MenuItems {
   }
 
   /******
-    Create and Display Sub Menus
+    Display Sub Menus
   ******/
 
-  sortMenuItemsforSecondaryMenu(item) {
-    if (item.hasAttribute("touch")) {
-      this.touchMenu.appendChild(item);
-    }
-    // should not exist
-    if (item.hasAttribute("matching")) {
-      this.matchingMenu.appendChild(item);
-    }
-    if (item.hasAttribute("fluency")) {
-      this.fluencyMenu.appendChild(item);
-    }
-    if (item.hasAttribute("writing")) {
-      this.writingMenu.appendChild(item);
-    }
-  }
   displaySecondaryMenu(section) {
     // this.removePrimaryMenu();
     this.hidePrimaryMenu();
@@ -458,34 +443,14 @@ class MenuItems {
     switch (section) {
       case "alphabet":
         topContainer.innerText = "Alphabet";
-        // this.displayAlphabetSubMenu();
         this.alphabetCapitalsCardTouchAppMenuItem.classList.remove("hidden");
         this.alphabetLowercaseCardTouchAppMenuItem.classList.remove("hidden");
         this.alphabetMatchingAppMenuItem.classList.remove("hidden");
-        // setTimeout(() => {
-        //   const alphabetAppItems = document.querySelectorAll(
-        //     `[app-content="alphabet"]`
-        //   );
-        //   console.log(alphabetAppItems);
-
-        //   alphabetAppItems.forEach((item) => {
-        //     this.sortMenuItemsforSecondaryMenu(item);
-        //   });
-        // }, 300);
-        menuItems.returnToMainMenuToggle();
         this.touchMenu.appendChild(this.alphabetCapitalsCardTouchAppMenuItem);
         this.touchMenu.appendChild(this.alphabetLowercaseCardTouchAppMenuItem);
         this.matchingMenu.appendChild(this.alphabetMatchingAppMenuItem);
         break;
       case "numbers":
-        // const numbersAppItems = document.querySelectorAll(
-        //   `[app-type="numbers-app"]`
-        // );
-        // console.log(numbersAppItems);
-
-        // numbersAppItems.forEach((item) => {
-        //   this.sortMenuItemsforSecondaryMenu(item);
-        // });
         this.fluencyMenu.appendChild(this.numberFluency1to20AppMenuItem);
         this.fluencyMenu.appendChild(this.numberFluency21to40AppMenuItem);
         this.fluencyMenu.appendChild(this.numberFluency41to60AppMenuItem);
@@ -500,308 +465,10 @@ class MenuItems {
         this.touchMenu.appendChild(this.sightWords2AppMenuItem);
         this.touchMenu.appendChild(this.sightWords3AppMenuItem);
         this.writingMenu.appendChild(this.sightWords1WritingAppMenuItem);
-        // const sightWordsAppItems = document.querySelectorAll(
-        //   `[app-type="sight-words-app"]`
-        // );
-        // console.log(sightWordsAppItems);
-
-        // sightWordsAppItems.forEach((item) => {
-        //   sortMenuItemsforSecondaryMenu(item);
-        // });
         break;
       case "letter-sounds":
-        // const letterSoundsAppItems = document.querySelectorAll(
-        //   `[app-type="letter-sounds-app"]`
-        // );
-        // console.log(letterSoundsAppItems);
-
-        // letterSoundsAppItems.forEach((item) => {
-        //   sortMenuItemsforSecondaryMenu(item);
-        // });
-        // this.letterSoundsMenu.appendChild(this.alphabetCapitalsCardTouchAppMenuItem)
-        // this.touchMenu.appendChild(this.alphabetLowercaseCardTouchAppMenuItem)
-        // this.matchingMenu.appendChild(this.alphabetMatchingAppMenuItem)
         break;
     }
-  }
-  createAlphabetMenu() {}
-  displayAlphabetSubMenu() {
-    // secondaryMenu = "alphabet";
-    isSecondaryMenu = false;
-    isTertiaryMenu = true;
-    mainMenuSfx.select2.play();
-    isPrimaryMenu = false;
-    hideParentsInfoBtn();
-    topContainer.innerText = "Alphabet";
-    this.abcMenu.classList.add("hidden");
-    this.numbersMenu.classList.add("hidden");
-    this.sightWordsMenu.classList.add("hidden");
-    this.letterSoundsMenu.classList.add("hidden");
-    primaryMenuContainer.appendChild(this.alphabetCapitalsCardTouchAppMenuItem);
-    primaryMenuContainer.appendChild(
-      this.alphabetLowercaseCardTouchAppMenuItem
-    );
-    primaryMenuContainer.appendChild(this.alphabetMatchingAppMenuItem);
-    primaryMenuContainer.appendChild(returnToMainMenuBtn);
-    this.alphabetCapitalsCardTouchAppMenuItem.classList.remove("hidden");
-    this.alphabetLowercaseCardTouchAppMenuItem.classList.remove("hidden");
-    this.alphabetMatchingAppMenuItem.classList.remove("hidden");
-    menuItems.returnToMainMenuToggle();
-  }
-  createSightWordsSubMenu() {
-    // 1. Sight Words 1 Touch App Menu Item
-    this.sightWords1AppMenuItem.setAttribute(
-      "id",
-      "sight-words-1-app-menu-item"
-    );
-    this.sightWords1AppMenuItem.setAttribute("app-type", "sight-words-app");
-    this.sightWords1AppMenuItem.classList.add("div", "secondary-menu-item");
-    this.sightWords1AppMenuItem.innerText = "Sight Words 1";
-    this.sightWords1AppMenuItem.addEventListener("click", () =>
-      appLauncher.startCardTouchApp("sightwords1")
-    );
-
-    // 2. Sight Words 2 Touch App Menu Item
-    this.sightWords2AppMenuItem.setAttribute(
-      "id",
-      "sight-words-2-app-menu-item"
-    );
-    this.sightWords2AppMenuItem.setAttribute("app-type", "sight-words-app");
-    this.sightWords2AppMenuItem.classList.add("div", "secondary-menu-item");
-    this.sightWords2AppMenuItem.innerText = "Sight Words 2";
-    this.sightWords2AppMenuItem.addEventListener("click", () =>
-      appLauncher.startCardTouchApp("sightwords2")
-    );
-
-    // 3. Sight Words 3 Touch App Menu Item
-    this.sightWords3AppMenuItem.setAttribute(
-      "id",
-      "sight-words-3-app-menu-item"
-    );
-    this.sightWords3AppMenuItem.setAttribute("app-type", "sight-words-app");
-    this.sightWords3AppMenuItem.classList.add("div", "secondary-menu-item");
-    this.sightWords3AppMenuItem.innerText = "Sight Words 3";
-    this.sightWords3AppMenuItem.addEventListener("click", () =>
-      appLauncher.startCardTouchApp("sightwords3")
-    );
-
-    // 4. Sight Words 1 Writing App Menu Item
-    this.sightWords1WritingAppMenuItem.setAttribute(
-      "id",
-      "sight-words-1-writing-app-menu-item"
-    );
-    this.sightWords1WritingAppMenuItem.setAttribute(
-      "app-type",
-      "sight-words-app"
-    );
-    this.sightWords1WritingAppMenuItem.classList.add(
-      "div",
-      "secondary-menu-item"
-    );
-    this.sightWords1WritingAppMenuItem.innerText = "Sight Words Writing";
-    this.sightWords1WritingAppMenuItem.addEventListener("click", () =>
-      appLauncher.startWritingApp("sightwords1")
-    );
-  }
-  displaySightWordsMenu() {
-    mainMenuSfx.select2.play();
-    isPrimaryMenu = false;
-    hideParentsInfoBtn();
-    topContainer.innerText = "Sight Words";
-    this.abcMenu.classList.add("hidden");
-    this.numbersMenu.classList.add("hidden");
-    this.sightWordsMenu.classList.add("hidden");
-    this.letterSoundsMenu.classList.add("hidden");
-    primaryMenuContainer.appendChild(this.sightWords1AppMenuItem);
-    primaryMenuContainer.appendChild(this.sightWords2AppMenuItem);
-    primaryMenuContainer.appendChild(this.sightWords3AppMenuItem);
-    primaryMenuContainer.appendChild(this.sightWords1WritingAppMenuItem);
-    primaryMenuContainer.appendChild(returnToMainMenuBtn);
-    this.sightWords1AppMenuItem.classList.remove("hidden");
-    this.sightWords2AppMenuItem.classList.remove("hidden");
-    this.sightWords3AppMenuItem.classList.remove("hidden");
-    this.sightWords1WritingAppMenuItem.classList.remove("hidden");
-    menuItems.returnToMainMenuToggle();
-  }
-  createLetterSoundsSubMenu() {
-    this.letterSoundsAMSFAppMenuItem.setAttribute("id", "amsf-app-menu-item");
-    this.letterSoundsAMSFAppMenuItem.classList.add(
-      "div",
-      "secondary-menu-item"
-    );
-    this.letterSoundsAMSFAppMenuItem.innerText = "a, m , s, f";
-    this.letterSoundsAMSFAppMenuItem.addEventListener("click", () =>
-      appLauncher.startCardTouchApp("letter-sounds-asmf")
-    );
-  }
-  displayLetterSoundsMenu() {
-    mainMenuSfx.select2.play();
-    isPrimaryMenu = false;
-    hideParentsInfoBtn();
-    topContainer.innerText = "Sight Words";
-    this.abcMenu.classList.add("hidden");
-    this.numbersMenu.classList.add("hidden");
-    this.sightWordsMenu.classList.add("hidden");
-    this.letterSoundsMenu.classList.add("hidden");
-    primaryMenuContainer.appendChild(this.letterSoundsAMSFAppMenuItem);
-    this.letterSoundsAMSFAppMenuItem.classList.remove("hidden");
-    menuItems.returnToMainMenuToggle();
-  }
-  createNumbersSubMenu() {
-    // 1. Number Fluency 1-20 App Menu Item
-    this.numberFluency1to20AppMenuItem.setAttribute(
-      "id",
-      "number-fluency-1-to-20-app-menu-item"
-    );
-    this.numberFluency1to20AppMenuItem.setAttribute("app-style", "numbers");
-    this.numberFluency1to20AppMenuItem.classList.add(
-      "div",
-      "secondary-menu-item"
-    );
-    this.numberFluency1to20AppMenuItem.innerText = "Touch 1-20";
-    this.numberFluency1to20AppMenuItem.addEventListener("click", () =>
-      appLauncher.startFluencyApp(1)
-    );
-
-    // 2. Number Fluency 21-40 App Menu Item
-    this.numberFluency21to40AppMenuItem.setAttribute(
-      "id",
-      "number-fluency-21-to-40-app-menu-item"
-    );
-    this.numberFluency21to40AppMenuItem.setAttribute("app-style", "numbers");
-    this.numberFluency21to40AppMenuItem.classList.add(
-      "div",
-      "secondary-menu-item"
-    );
-    this.numberFluency21to40AppMenuItem.innerText = "Touch 21-40";
-    this.numberFluency21to40AppMenuItem.addEventListener("click", () =>
-      appLauncher.startFluencyApp(2)
-    );
-
-    // 3. Number Fluency 41-60 App Menu Item
-    this.numberFluency41to60AppMenuItem.setAttribute(
-      "id",
-      "number-fluency-41-to-60-app-menu-item"
-    );
-    this.numberFluency41to60AppMenuItem.setAttribute("app-style", "numbers");
-    this.numberFluency41to60AppMenuItem.classList.add(
-      "div",
-      "secondary-menu-item"
-    );
-    this.numberFluency41to60AppMenuItem.innerText = "Touch 41-60";
-    this.numberFluency41to60AppMenuItem.addEventListener("click", () =>
-      appLauncher.startFluencyApp(3)
-    );
-
-    // 4. Number Fluency 61-80 App Menu Item
-    this.numberFluency61to80AppMenuItem.setAttribute(
-      "id",
-      "number-fluency-61-to-80-app-menu-item"
-    );
-    this.numberFluency61to80AppMenuItem.setAttribute("app-style", "numbers");
-    this.numberFluency61to80AppMenuItem.classList.add(
-      "div",
-      "secondary-menu-item"
-    );
-    this.numberFluency61to80AppMenuItem.innerText = "Touch 61-80";
-    this.numberFluency61to80AppMenuItem.addEventListener("click", () =>
-      appLauncher.startFluencyApp(4)
-    );
-
-    // 5. Number Fluency 81-100 App Menu Item
-    this.numberFluency81to100AppMenuItem.setAttribute(
-      "id",
-      "number-fluency-81-to-100-app-menu-item"
-    );
-    this.numberFluency81to100AppMenuItem.setAttribute("app-style", "numbers");
-    this.numberFluency81to100AppMenuItem.classList.add(
-      "div",
-      "secondary-menu-item"
-    );
-    this.numberFluency81to100AppMenuItem.innerText = "Touch 81-100";
-    this.numberFluency81to100AppMenuItem.addEventListener("click", () =>
-      appLauncher.startFluencyApp(5)
-    );
-    // 6. Number Writing 1-10 App Menu Item
-    this.numberWriting1to10AppMenuItem.setAttribute(
-      "id",
-      "number-writing-1-to-10-app-menu-item"
-    );
-    this.numberWriting1to10AppMenuItem.setAttribute("app-style", "numbers");
-    this.numberWriting1to10AppMenuItem.classList.add(
-      "div",
-      "secondary-menu-item"
-    );
-    this.numberWriting1to10AppMenuItem.innerText = "Writing 1-10";
-    this.numberWriting1to10AppMenuItem.addEventListener("click", () =>
-      appLauncher.startWritingApp("numbers1-10")
-    );
-    // 7. Number Writing 11-20 App Menu Item
-    this.numberWriting11to20AppMenuItem.setAttribute(
-      "id",
-      "number-writing-11-to-20-app-menu-item"
-    );
-    this.numberWriting11to20AppMenuItem.setAttribute("app-style", "numbers");
-    this.numberWriting11to20AppMenuItem.classList.add(
-      "div",
-      "secondary-menu-item"
-    );
-    this.numberWriting11to20AppMenuItem.innerText = "Writing 11-20";
-    this.numberWriting11to20AppMenuItem.addEventListener("click", () =>
-      appLauncher.startWritingApp("numbers11-20")
-    );
-    // 8. Number Writing 21-40 App Menu Item
-    this.numberWriting21to40AppMenuItem.setAttribute(
-      "id",
-      "number-writing-21-to-40-app-menu-item"
-    );
-    this.numberWriting21to40AppMenuItem.setAttribute("app-style", "numbers");
-    this.numberWriting21to40AppMenuItem.classList.add(
-      "div",
-      "secondary-menu-item"
-    );
-    this.numberWriting21to40AppMenuItem.innerText = "Writing 21-40";
-    this.numberWriting21to40AppMenuItem.addEventListener("click", () =>
-      appLauncher.startWritingApp("numbers21-40")
-    );
-    // 9. Number Writing 1-50 App Menu Item
-    this.numberWriting1to50AppMenuItem.setAttribute(
-      "id",
-      "number-writing-1-to-50-app-menu-item"
-    );
-    this.numberWriting1to50AppMenuItem.setAttribute("app-style", "numbers");
-    this.numberWriting1to50AppMenuItem.classList.add(
-      "div",
-      "secondary-menu-item"
-    );
-    this.numberWriting1to50AppMenuItem.innerText = "Writing 1-50";
-    this.numberWriting1to50AppMenuItem.addEventListener("click", () =>
-      appLauncher.startWritingApp("numbers1-50")
-    );
-  }
-  displayNumbersMenu() {
-    mainMenuSfx.select2.play();
-    isPrimaryMenu = false;
-    hideParentsInfoBtn();
-    topContainer.innerText = "Numbers";
-    this.abcMenu.classList.add("hidden");
-    this.numbersMenu.classList.add("hidden");
-    this.sightWordsMenu.classList.add("hidden");
-    primaryMenuContainer.appendChild(this.numberFluency1to20AppMenuItem);
-    primaryMenuContainer.appendChild(this.numberFluency21to40AppMenuItem);
-    primaryMenuContainer.appendChild(this.numberFluency41to60AppMenuItem);
-    primaryMenuContainer.appendChild(this.numberFluency61to80AppMenuItem);
-    primaryMenuContainer.appendChild(this.numberFluency81to100AppMenuItem);
-    primaryMenuContainer.appendChild(this.numberWriting1to10AppMenuItem);
-    primaryMenuContainer.appendChild(this.numberWriting11to20AppMenuItem);
-    primaryMenuContainer.appendChild(this.numberWriting21to40AppMenuItem);
-    primaryMenuContainer.appendChild(this.numberWriting1to50AppMenuItem);
-    primaryMenuContainer.appendChild(returnToMainMenuBtn);
-    this.numberFluency1to20AppMenuItem.classList.remove("hidden");
-    this.numberFluency21to40AppMenuItem.classList.remove("hidden");
-    this.numberFluency41to60AppMenuItem.classList.remove("hidden");
-    this.numberFluency61to80AppMenuItem.classList.remove("hidden");
-    this.numberFluency81to100AppMenuItem.classList.remove("hidden");
     menuItems.returnToMainMenuToggle();
   }
 
@@ -968,6 +635,7 @@ function displayGreeting() {
 
 function displayMainPage() {
   isPrimaryMenu = true;
+  isSecondaryMenu = false;
   menuItems.returnToMainMenuToggle();
   const navBarDisplay = `${user.firstName} ${user.lastName.slice(0, 1)}.`;
   navLogo.innerText = `KGPS English Practice`;
