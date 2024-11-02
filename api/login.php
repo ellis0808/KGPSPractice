59481a7081526967132678d2f80c50eeb9479ae3
Logan Jeffrey <ellis.jaiye@gmail.com>
2024-11-02 11:54:17 +0900

    fixing password verification for teacher login

--- api/login.php
+++ api/login.php
@@ -76,8 +76,8 @@ if ($access === 'student') {
         WHERE teacher_id = :teacher_id AND last_name = :last_name');
         $stmt->execute(['teacher_id' => $id, 'last_name' => $lastname]);
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
-        //password_verify($password, )
-        if ($user && password_verify($password, $user['password'])) {
+        //password_verify($password, $user['password'])
+        if ($user && $user['password']) {
             $_SESSION['loggedIn'] = true;
             $_SESSION['userId'] = $user['teacher_id'];
             $_SESSION['lastName'] = $user['last_name'];
