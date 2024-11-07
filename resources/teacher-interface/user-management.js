28c7b261c54e7115ff49e340dc320821db9ff01d
Logan Jeffrey <ellis.jaiye@gmail.com>
2024-11-07 22:23:39 +0900

    updating table headers to display correctly in user-management.js

--- resources/teacher-interface/user-management.js
+++ resources/teacher-interface/user-management.js
@@ -78,11 +78,6 @@ const displayUsers = {
     this.accessHeader.textContent = "Access";
     this.gradeLevelHeader.textContent = "Grade";
     this.userDataHeader.classList.add("user-data-header");
-    this.userDataHeader.appendChild(this.numberHeader);
-    this.userDataHeader.appendChild(this.nameHeader);
-    this.userDataHeader.appendChild(this.gradeLevelHeader);
-    this.userDataHeader.appendChild(this.accessHeader);
-    userList.appendChild(this.userDataHeader);
   },
   appendUserDataHeaders() {
     this.userDataHeader.appendChild(this.numberHeader);
@@ -210,8 +205,8 @@ const displayUsers = {
     userList.textContent = "";
     // this.i = 0;
     this.createTableHeaders();
-    this.createUserDataRowElements();
-    // this.appendUserDataHeaders();
+    // this.createUserDataRowElements();
+    this.appendUserDataHeaders();
     // this.setStudentDataInRows(data.students);
     // this.setTeacherDataInRows(data.teachers);
     let i = 0;
