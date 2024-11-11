1fefca9629cdeda2d5239612e97cfb58b44113da
Logan Jeffrey <ellis.jaiye@gmail.com>
2024-11-11 22:48:14 +0900

    setting up session check for teacher login

--- api/session-check.php
+++ api/session-check.php
@@ -11,14 +11,18 @@ header('Content-Type: application/json');
 
 
 if (isset($_SESSION['loggedIn'])) {
-    echo json_encode([
-        'loggedIn' => true,
-        'userId' => $_SESSION['userId'],
-        'firstName' => $_SESSION['firstName'],
-        'lastName' => $_SESSION['lastName'],
-        'access' => $_SESSION['access'],
-        'gradeLevel' => $_SESSION['gradeLevel']
-    ]);
+    echo json_encode($_SESSION['access']);
+    exit;
+    // if ($_SESSION['access'] === 'student') {
+    //     echo json_encode([
+    //         'loggedIn' => true,
+    //         'userId' => $_SESSION['userId'],
+    //         'firstName' => $_SESSION['firstName'],
+    //         'lastName' => $_SESSION['lastName'],
+    //         'access' => $_SESSION['access'],
+    //         'gradeLevel' => $_SESSION['gradeLevel']
+    //     ]);
+    // } elseif ($_SESSION['access'] === 'teacher') {}
 } else {
     echo json_encode(['loggedIn' => false]);
 }
