b9998d1e839679e602c5e087c3d5b01856d75f61
Logan Jeffrey <ellis.jaiye@gmail.com>
2024-12-20 22:52:10 +0900

    writing timer display fix

--- resources/utilities/timer.js
+++ resources/utilities/timer.js
@@ -44,11 +44,13 @@ const timerFunction = {
           ++this.time;
           this.updateTimerDisplay();
 
-          // } else if (this.time >= 10) {
-          //   this.timer.textContent = `0:${this.time}`;
-          //   if (this.time > 59) {
-          //   }
-          // }
+          const m = Math.floor(this.time / 60);
+          const s = this.time % 60;
+          if (s === 0) {
+            this.timer.textContent = `${m}:0${s}`;
+          } else {
+            this.timer.textContent = `${m}:${s}`;
+          }
         }
       }, 1000);
     }, 1200);
