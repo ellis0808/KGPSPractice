035070b6142d54e6af36a9e9b0cd3fe51836ae3f
Logan Jeffrey <ellis.jaiye@gmail.com>
2024-12-25 01:26:43 +0900

    hiding password ahead of opening up github to public

--- api/db_connect.php
+++ api/db_connect.php
@@ -9,11 +9,10 @@ if (session_status() == PHP_SESSION_NONE) {
 
 function getDBConnection()
 {
-    $config = require 'config.php';
-    $host = $config['DB_HOST'];
-    $db = $config['DB_NAME'];
-    $user = $config['DB_USER'];
-    $pass = $config['DB_PASSWORD'];
+    $host = getenv('DB_HOST');
+    $db = getenv('DB_NAME');
+    $user = getenv('DB_USER');
+    $pass = getenv('DB_PASSWORD');
     $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
     $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES => false,];
 
