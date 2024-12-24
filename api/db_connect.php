f377cfa2eb32bbb6b0c51dc1486ac932ba5b5c68
Logan Jeffrey <ellis.jaiye@gmail.com>
2024-12-25 01:05:39 +0900

    hiding password ahead of opening up github to public

--- api/db_connect.php
+++ api/db_connect.php
@@ -4,23 +4,16 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-// Redirect to login if the user is not logged in
-// if (!isset($_SESSION['user_id']) && basename($_SERVER['PHP_SELF']) != 'login.php') {
-//     header('Location: ../resources/login/login.html');
-//     exit();
-// }
-
-
-
 
 
 
 function getDBConnection()
 {
-    $host = 'mysql80.orchidpony8.sakura.ne.jp';
-    $db = 'orchidpony8_kgps_eng_pr';
-    $user = 'orchidpony8';
-    $pass = 'he1-maha_ptic74';
+    $config = require 'config.php';
+    $host = $config['DB_HOST'];
+    $db = $config['DB_NAME'];
+    $user = $config['DB_USER'];
+    $pass = $config['DB_PASSWORD'];
     $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
     $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES => false,];
 
