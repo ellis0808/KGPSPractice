82b413e660ea85528a4727c3da6a2469cbd77479
Logan Jeffrey <ellis.jaiye@gmail.com>
2025-01-03 20:15:13 +0900

    fixing sessions file saving location... needed to be different for each environment

--- api/session_check.php
+++ api/session_check.php
@@ -2,21 +2,21 @@
 
 header('Content-Type: application/json');
 
-$env = getenv('APP_ENV') ?: 'production';
-switch ($env) {
-    case 'dev':
-        ini_set('session.save_path', '/KGPSEnglishPractice-dev/sessions');
-        break;
-    case 'preview':
-        ini_set('session.save_path', '/KGPSEnglishPractice-preview/sessions');
-        break;
-    case 'production':
-        ini_set('session.save_path', '/KGPSEnglishPractice/sessions');
-        break;
-    default:
-        error_log('Unknown environment: $env');
-        exit('Environment configuration error.');
-}
+// $env = getenv('APP_ENV') ?: 'production';
+// switch ($env) {
+//     case 'dev':
+//         ini_set('session.save_path', '/KGPSEnglishPractice-dev/sessions');
+//         break;
+//     case 'preview':
+//         ini_set('session.save_path', '/KGPSEnglishPractice-preview/sessions');
+//         break;
+//     case 'production':
+//         ini_set('session.save_path', '/KGPSEnglishPractice/sessions');
+//         break;
+//     default:
+//         error_log('Unknown environment: $env');
+//         exit('Environment configuration error.');
+// }
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
