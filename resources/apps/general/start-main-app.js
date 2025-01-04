06fa9b2ac0a674377a1d9a3430115d42b20b31d7
Logan Jeffrey <ellis.jaiye@gmail.com>
2025-01-04 22:00:44 +0900

    fix for secondary menu item persistence after quitting app

--- resources/apps/general/start-main-app.js
+++ resources/apps/general/start-main-app.js
@@ -600,6 +600,8 @@ class MenuItems {
   removeSectionColumnMenuItems() {
     this.sectionColumn.childNodes.forEach((menu) => {
       menu.childNodes.forEach((item) => {
+        console.log(item);
+
         item.remove();
       });
     });
