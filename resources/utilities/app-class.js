99e5e383d2d7dcae89a1f413c41ae3f0e7767e9e
Logan Jeffrey <ellis.jaiye@gmail.com>
2025-01-06 22:35:12 +0900

    different approach for removing app elements after session ends - selecting each one by class individually then attempting remove

--- resources/utilities/app-class.js
+++ resources/utilities/app-class.js
@@ -179,7 +179,7 @@ class App {
     this.appContainer.classList.add("hide");
     this.appControlsContainer.classList.add("hide");
     console.log(this.endSessionItems);
-    document.querySelectorAll(this.endSessionItems[0]).forEach((item) => {
+    document.querySelectorAll(`${this.endSessionItems[0]}`).forEach((item) => {
       console.log(item);
       item.remove();
     });
