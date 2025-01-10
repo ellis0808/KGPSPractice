8329a7e2bd45fab509f38d4349aa11fa5629d9e4
Logan Jeffrey <ellis.jaiye@gmail.com>
2025-01-10 21:47:49 +0900

    fixing info sent to database for activity records

--- api/add_user_activity_record.php
+++ api/add_user_activity_record.php
@@ -14,6 +14,7 @@ $userId = $data['user_id'] ?? '';
 $userType = $data['user_type'] ?? '';
 $correctAnswerCount = $data['correct_answer_count'] ?? '';
 $incorrectAnswerCount = $data['incorrect_answer_count'] ?? '';
+$totalElapsedTime = $data['total_elapsed_time'] ?? '';
 $timeToCorrectAnswerDurationInSeconds = $data['time_to_correct_answer_duration_in_seconds'] ?? '';
 $answerAttempts = $data['answer_attempts'] ?? '';
 $activityScore = $data['activity_score'] ?? '';
@@ -22,8 +23,8 @@ $timeStamp = date('Y-m-d H:i:s');
 try {
     $pdo = getDBConnection();
 
-    $stmt = $pdo->prepare('INSERT INTO activity_stats (activity_id, user_id, user_type, correct_answer_count, incorrect_answer_count, time_to_correct_answer_duration_in_seconds, answer_attempts, activity_score, date_time) VALUES (:activity_id, :user_id, :user_type, :correct_answer_count, :incorrect_answer_count, :time_to_correct_answer_duration_in_seconds, :answer_attempts, :activity_score, :date_time)');
-    $stmt->execute(['activity_id' => $activityId, 'user_id' => $userId, 'user_type' => $userType, 'correct_answer_count' => $correctAnswerCount, 'incorrect_answer_count' => $incorrectAnswerCount, 'time_to_correct_answer_duration_in_seconds' => $timeToCorrectAnswerDurationInSeconds, 'answer_attempts' => $answerAttempts, 'activity_score' => $activityScore, 'date_time' => $timeStamp]);
+    $stmt = $pdo->prepare('INSERT INTO activity_stats (activity_id, user_id, user_type, correct_answer_count, incorrect_answer_count, total_elapsed_time, time_to_correct_answer_duration_in_seconds, answer_attempts, activity_score, date_time) VALUES (:activity_id, :user_id, :user_type, :correct_answer_count, :incorrect_answer_count, :total_elapsed_time, :time_to_correct_answer_duration_in_seconds, :answer_attempts, :activity_score, :date_time)');
+    $stmt->execute(['activity_id' => $activityId, 'user_id' => $userId, 'user_type' => $userType, 'correct_answer_count' => $correctAnswerCount, 'incorrect_answer_count' => $incorrectAnswerCount, 'total_elpased_time' => $totalElapsedTime, 'time_to_correct_answer_duration_in_seconds' => $timeToCorrectAnswerDurationInSeconds, 'answer_attempts' => $answerAttempts, 'activity_score' => $activityScore, 'date_time' => $timeStamp]);
 
     echo     json_encode(['message' => 'record added successfully']);
 } catch (PDOException $e) {
