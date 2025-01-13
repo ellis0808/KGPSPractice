a8558135aecbc8173726f4d6d0ce4063be231509
Logan Jeffrey <ellis.jaiye@gmail.com>
2025-01-13 11:31:48 +0900

    tweaks to previous update

--- resources/apps/matching/dot-objects-control.js
+++ resources/apps/matching/dot-objects-control.js
@@ -232,7 +232,9 @@ class EndDot {
   markAsIncorrect() {
     this.removeCorrectPulse();
     matchingApp.incorrectShortAnswersArray.push(this.contentId);
-    matchingApp.incorrectShortAnswersArray.push(this.connectedTo.contentId);
+    matchingApp.incorrectShortAnswersArray.push(
+      this.connectedTo.element.contentId
+    );
   }
 }
 
