f60e0db93d898926346c482957870413d073efd9
Logan Jeffrey <ellis.jaiye@gmail.com>
2025-01-15 23:06:41 +0900

    tweaks to previous update

--- resources/apps/general/start-main-app.js
+++ resources/apps/general/start-main-app.js
@@ -1121,9 +1121,9 @@ class MenuItems {
   scrollRight() {
     let i = 0;
     const step = () => {
-      this.sectionColumn.scrollLeft += 10;
+      this.sectionColumn.scrollLeft += 12;
 
-      if (i < 250) {
+      if (i < 225) {
         i += 2;
         requestAnimationFrame(step);
       } else {
@@ -1140,9 +1140,9 @@ class MenuItems {
   scrollLeft() {
     let i = 0;
     const step = () => {
-      this.sectionColumn.scrollLeft += -10;
+      this.sectionColumn.scrollLeft += -12;
 
-      if (i < 250) {
+      if (i < 225) {
         i += 2;
         requestAnimationFrame(step);
       } else {
