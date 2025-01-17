f65afe27617a02b1ef4375849432c913c16d9394
Logan Jeffrey <ellis.jaiye@gmail.com>
2025-01-17 23:14:54 +0900

    tweaks to previous update

--- resources/login/login.js
+++ resources/login/login.js
@@ -286,9 +286,16 @@ const selectImage = (item) => {
   console.log(studentSelectedPasswordImage1);
   console.log(studentSelectedPasswordImage2);
   console.log(studentPasswordEntryArray);
-  if (!studentSelectedPasswordImage1.src) {
+  if (
+    !studentSelectedPasswordImage1.src ||
+    !studentSelectedPasswordImage2.src
+  ) {
     studentSelectedPasswordImage1.src = item.src;
     console.log("test");
+  }
+  if (!studentSelectedPasswordImage2.src) {
+    studentSelectedPasswordImage2.src = item.src;
+    console.log("test2");
   }
 };
 studentPasswordGridContainer.appendChild(studentPasswordGrid);
