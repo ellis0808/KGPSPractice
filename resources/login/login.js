888124e264ade796e0ef3a3f2e7000e867d43347
Logan Jeffrey <ellis.jaiye@gmail.com>
2025-01-17 22:49:03 +0900

    tweaks to previous update

--- resources/login/login.js
+++ resources/login/login.js
@@ -308,7 +308,10 @@ const studentPasswordEntry = () => {
           console.log(event.target);
           console.log("condition 2b");
         }
-        if (studentPasswordEntryArray.length === 2) {
+        if (
+          studentSelectedPasswordImage1.src ||
+          studentSelectedPasswordImage2.src
+        ) {
           studentPasswordEntryArray.splice(0, 1);
           // displaySelectedPasswordImages(item);
           studentSelectedPasswordImage2.src = studentSelectedPasswordImage1.src;
